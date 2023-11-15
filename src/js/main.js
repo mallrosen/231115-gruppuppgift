f853ebf4cdbe6b259e58524b51d49552ef1de605
Andreas Ishak <Andreas.Ishak@medieinstitutet.se>
2023-11-15 14:39:31 +0100

    added list

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,32 @@
-import './../scss/style.scss'
+import "./../scss/style.scss";
+
+class Bilar {
+  brand;
+  speed;
+  constructor(brand, speed) {
+    this.brand = brand;
+    this.speed = speed;
+  }
+}
+
+const app = document.getElementById("app");
+const div = document.createElement("div");
+
+const bilar = new Bilar("volvo", "320km/h");
+const bilar2 = new Bilar("mercedes", "500km/h");
+const vroom = [bilar, bilar2];
+for (let i = 0; i < vroom.length; i++) {
+  const paragraph = document.createElement("p");
+  const paragraph2 = document.createElement("p");
+
+  paragraph.innerHTML = vroom[i].brand;
+  paragraph2.innerHTML = vroom[i].speed;
+
+  paragraph.className = "paragraph1";
+  paragraph2.className = "paragraph2";
+
+  div.appendChild(paragraph);
+  div.appendChild(paragraph2);
+}
+
+document.body.appendChild(div);
