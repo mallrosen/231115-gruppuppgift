032eaf288a45c7a87af2687281a9d1262f8d2b2f
malin <malin.rosen@medieinstitutet.se>
2023-11-15 14:40:10 +0100

    malins lista

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
-import './../scss/style.scss'
+import "./../scss/style.scss";
+import { Animals } from "./models/Animals";
+
+const a1 = new Animals("Hund ", "Pluto ", 11);
+const a2 = new Animals("Katt ", "Boris ", 1);
+const a3 = new Animals("Kanin ", "Skutt ", 2);
+
+const animalList = [a1, a2, a3]
+const ul = document.createElement("ul");
+document.body.appendChild(ul)
+
+for (let i = 0; i < animalList.length; i++) {
+  const li = document.createElement("li");
+  ul.appendChild(li);
+  li.innerHTML = animalList[i].art + animalList[i].name + animalList[i].age ;
+  li.className = "animalsLi";
+}
+
+ul.className = "animalsUl";
+
