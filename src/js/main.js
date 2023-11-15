8f0c76f9b4dfd74b607cc780daff48eb32127668
Leonard Deutgen <leonard.deutgen@medieinstitutet.se>
2023-11-15 14:10:21 +0100

    firstLeoCommit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,28 @@
 import './../scss/style.scss'
+
+class Products{
+    name;
+    brand;
+    price;
+    constructor(name,brand,price){
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+    }
+}
+
+ const mobile = new Products("Mobil","Iphone",7000);
+ const computer = new Products("Dator","Mac",21000);
+ const headphones = new Products("Hörlurar","Bose",2000);
+
+ let productsList = [mobile,computer,headphones];
+
+ for (let i=0; i<productsList.length; i++){
+    const ul = document.createElement("ul");
+    const li = document.createElement("li");
+
+    li.innerHTML = productsList[i].name;
+    ul.appendChild(li);
+ }
+
+ document.body.appendChild(ul);
